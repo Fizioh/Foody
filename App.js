1f4902049a4eb3c6b7cd19e4ec444e337352787d
Fizioh <batrom@laposte.net>
2021-04-24 12:38:06 +0200

    set screens in app

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {createStackNavigator} from '@react-navigation/stack'
+import {NavigationContainer} from '@react-navigation/native'
+import {Home, Restaurant, OrderDelivery} from './screens'
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+      initialRouteName={'Home'}
+      >
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Restaurant' component={Restaurant} />
+        <Stack.Screen name='OrderDelivery' component={OrderDelivery} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
