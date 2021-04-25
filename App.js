b27462ee612219be68f31179a53b2b283f3ae1fc
Fizioh <batrom@laposte.net>
2021-04-25 12:31:19 +0200

    complete with 3 more tabs

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {createStackNavigator} from '@react-navigation/stack'
 import {NavigationContainer} from '@react-navigation/native'
-import {Home, Restaurant, OrderDelivery} from './screens'
+import { Restaurant, OrderDelivery} from './screens'
 import Tabs from './navigaton/tabs'
 const Stack = createStackNavigator();
 
--- navigaton/tabs.js
+++ navigaton/tabs.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { View, Image, TouchableOpcity } from 'react-native'
+import { View, Image, TouchableOpacity } from 'react-native'
 import { createBottomTabNavigator, BottomTabBar } from '@react-navigation/bottom-tabs'
 import { Home } from '../screens'
 import { COLORS, icons } from '../constants'
 
-const Tab = createBottomTabNavigatior();
+const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
     return(
@@ -13,22 +13,73 @@ const Tabs = () => {
             name='Home'
             component={Home}
             options={{
-                tabBarIcon: ({focused}) => {
+                tabBarIcon: ({ focused }) => (
                     <Image
                         source={icons.cutlery}
                         resizeMode="contain"
                         style={{
                             width: 25,
                             height: 25,
-                            tintColor: focused ? COLORS.primary : COLORS.secondary
+                            tintColor: focused ?  COLORS.primary : COLORS.secondary 
                         }}
                     />
-                }
+                )
+            }}
+            />
+            <Tab.Screen
+            name='Like'
+            component={Home}
+            options={{
+                tabBarIcon: ({ focused }) => (
+                    <Image
+                        source={icons.like}
+                        resizeMode="contain"
+                        style={{
+                            width: 25,
+                            height: 25,
+                            tintColor: focused ?  COLORS.primary : COLORS.secondary 
+                        }}
+                    />
+                )
+            }}
+            />
+            <Tab.Screen
+            name='User'
+            component={Home}
+            options={{
+                tabBarIcon: ({ focused }) => (
+                    <Image
+                        source={icons.user}
+                        resizeMode="contain"
+                        style={{
+                            width: 25,
+                            height: 25,
+                            tintColor: focused ?  COLORS.primary : COLORS.secondary 
+                        }}
+                    />
+                )
+            }}
+            />
+            <Tab.Screen
+            name='Home'
+            component={Home}
+            options={{
+                tabBarIcon: ({ focused }) => (
+                    <Image
+                        source={icons.cutlery}
+                        resizeMode="contain"
+                        style={{
+                            width: 25,
+                            height: 25,
+                            tintColor: focused ?  COLORS.primary : COLORS.secondary 
+                        }}
+                    />
+                )
             }}
             />
         </Tab.Navigator>
     )
 }
 
-export default Tabs;
+export default Tabs
 
