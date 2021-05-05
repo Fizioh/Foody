3bca85841f491ff02da6f0be9fd4b7469fc03d07
Fizioh <batrom@laposte.net>
2021-05-05 20:17:10 +0200

    add restaurants views

--- screens/Home.js
+++ screens/Home.js
@@ -463,10 +463,84 @@ const Home = () => {
         )
     }
 
+    function renderRestaurantList() {
+        const renderItem = ({item}) => (
+                <TouchableOpacity
+                style={{marginBottom: SIZES.padding * 2}}
+                //onPress ->navigate to Restaurant screen
+                >
+                    <View
+                        style={{
+                            marginBottom: SIZES.padding
+                        }}
+                    >
+                        <Image 
+                            source={item.photo}
+                            resizeMode="cover"
+                            style={{
+                                width: "100%",
+                                height: 200,
+                                borderRadius: SIZES.radius
+                            }}
+                        />
+                        <View
+                            style={{
+                                position: 'absolute',
+                                bottom: 0,
+                                height: 50,
+                                width: SIZES.width * 0.3,
+                                backgroundColor: COLORS.white,
+                                borderTopRightRadius: SIZES.radius,
+                                borderBottomLeftRadius: SIZES.radius,
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                ...styles.shadow
+                            }}
+                        >
+                            <Text style={{ ...FONTS.h4 }}>{item.duration}</Text>
+                        </View>
+                    </View>
+                            {/* Restaurant Info */}
+                            <Text style={{...FONTS.body2 }}>{item.name}</Text>
+                        <View
+                            style={{
+                                marginTop: SIZES.padding,
+                                flexDirection: 'row'
+                            }}
+                        > 
+                        {/* Notes */}
+                            <Image 
+                                source={icons.star}
+                                style={{
+                                    height: 20,
+                                    width: 20,
+                                    tintColor: COLORS.primary,
+                                    marginRight: 10
+                                }}
+                            />
+                            <Text style={{...FONTS.body3}}>{item.rating}</Text>
+                        </View>
+                </TouchableOpacity>
+        )
+
+        return (
+            <FlatList 
+                data={restaurants}
+                keyExtractor={item => `${item.id}`}
+                renderItem={renderItem}
+                contentContainerStyle={{
+                    paddingHorizontal: SIZES.padding * 2,
+                    paddingBottom: 30
+                }}
+            />
+        )
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             {renderHeader()} 
-            {renderMainCategories()}   
+            {renderMainCategories()}
+            {renderRestaurantList()}
         </SafeAreaView>
     )
 }
