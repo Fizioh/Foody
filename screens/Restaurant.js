5e9be8929cb0d0f7e657fea4d1fa10152cdecab2
Fizioh <batrom@laposte.net>
2021-05-08 14:28:27 +0200

    add render food function

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -83,10 +83,123 @@ const Restaurant = ({ route, navigation }) => {
             </View>
         )
     }
+    function renderFoodInfo(){
+        return (
+            <Animated.ScrollView
+            horizontal
+            pagingEnabled
+            scrollEventThrottle={16}
+            snapToAlignement="center"
+            showsHorizontalScrollIndicator={false}
+            //onScroll
+            >
+               {
+                   restaurant?.menu.map((item, index) => (
+                       <View
+                        key={`menu-${index}`}
+                        style={{ alignItems: 'center', top: 30 }}
+                       >
+                           <View style={{ height: SIZES.height * 0.35 }}>
+                                {/* Food Image */}
+                                <Image 
+                                    source={item.photo}
+                                    resizeMode="cover"
+                                    style={{
+                                        width: SIZES.width,
+                                        height: "100%"
+                                    }}
+                                />
+                                {/* Quantity */}
+                                <View
+                                    style={{
+                                        position: 'absolute',
+                                        bottom: -20,
+                                        width: SIZES.width,
+                                        height: 50,
+                                        justifyContent: 'center',
+                                        flexDirection: 'row'
+                                    }}
+                                >
+                                    <TouchableOpacity
+                                        style={{
+                                            width: 50,
+                                            backgroundColor: COLORS.white,
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            borderTopLeftRadius: 25,
+                                            borderBottomLeftRadius: 25
+
+                                        }}
+                                    >
+                                        <Text style={{ ...FONTS.body1 }}>-</Text>
+                                    </TouchableOpacity>
+
+                                    <View
+                                        style={{
+                                            width: 50,
+                                            backgroundColor: COLORS.white,
+                                            alignItems: 'center',
+                                            justifyContent: 'center'
+                                        }}
+                                    >
+                                        <Text style={{ ...FONTS.h2 }}>1</Text>
+                                    </View>
+                                    <TouchableOpacity
+                                        style={{
+                                            width: 50,
+                                            backgroundColor: COLORS.white,
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                            borderTopRightRadius: 25,
+                                            borderBottomRightRadius: 25,
+                                        }}
+                                    >
+                                        <Text style={{ ...FONTS.body1 }}>+</Text>
+                                    </TouchableOpacity>
+                                </View>
+                           </View>
+                           {/*Nom & Description */}
+                           <View
+                            style={{
+                                width: SIZES.width,
+                                alignitems: 'center',
+                                marginTop: 15,
+                                paddingHorizontal: SIZES.padding * 2
+                            }}
+                           >
+                            <Text
+                                style={{ marginVertical: 10, textAlign: 'center', ...FONTS.h2 }} >{item.name} - {item.price.toFixed(2)}</Text>
+                            <Text style={{ ...FONTS.body3, textAlign: 'center' }}>{item.description}</Text>
+                           </View>
+
+                           {/* Calories */}
+                           <View
+                            style={{
+                                flexDirection: 'row',
+                                marginTop: 10
+                            }}
+                           >
+                               <Image 
+                                    source={icons.fire}
+                                    style={{
+                                        width: 20,
+                                        height: 20,
+                                        marginRight: 10
+                                    }}
+                               />
+                               <Text style={{ ...FONTS.body3, color: COLORS.darkgray}}>{item.calories.toFixed(2)} kcal</Text>
+                           </View>
+                       </View>
+                   ))
+               }
+            </Animated.ScrollView>
+        )
+    }
 
     return (
         <SafeAreaView style={styles.container}>
             {renderHeader()}
+            {renderFoodInfo()}
         </SafeAreaView>
     )
 }
