6c9f8139c307d72fd0667dd1a91af2f5ed13240b
Fizioh <batrom@laposte.net>
2021-05-09 11:47:51 +0200

    add order quantity function

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, View, Text, TouchableOpacity, Image, Animated } from 'react-native';
 import { isIphoneX } from 'react-native-iphone-x-helper';
+import { getFontScale } from 'react-native/Libraries/Utilities/PixelRatio';
 import { icons, COLORS, SIZES, FONTS } from '../constants'
 
 const Restaurant = ({ route, navigation }) => {
@@ -8,6 +9,7 @@ const Restaurant = ({ route, navigation }) => {
     const scrollX = new Animated.Value(0);
     const [restaurant, setRestaurant] = React.useState(null);
     const [currentLocation, setCurrentLocation] = React.useState(null);
+    const [orderItems, setOrderItems] = React.useState([]);
 
     React.useEffect(() => {
         let {item, currentLocation} = route.params;
@@ -16,6 +18,40 @@ const Restaurant = ({ route, navigation }) => {
         setCurrentLocation(currentLocation)
 
     })
+
+    function editOrder(action, menuId, price){
+        if(action == "+"){
+            let orderList = orderItems.slice()
+            let item = orderList.filter(a = a.menuId == menuId)
+
+            if(item.length > 0){
+                let newQty = item[0].qty+1
+                item[0].qty = newQty
+                item[0].total = item[0].qty * price
+            } else {
+                const newItem = {
+                    menuId: menuId,
+                    qty: 1,
+                    price: price,
+                    total: price
+                }
+                orderList.push(newItem)
+            }
+
+            setOrderItems(orderList)
+        } else {
+
+        }
+    }
+
+    function getOrderQty(menuId){
+        let orderItem = orderItems.filter(a => a.menuId == menuId)
+
+        if(orderItem.length > 0){
+            return orderItem[0].qty
+        }
+        return 0
+    }
 
     function renderHeader(){
         return (
@@ -145,7 +181,7 @@ const Restaurant = ({ route, navigation }) => {
                                             justifyContent: 'center'
                                         }}
                                     >
-                                        <Text style={{ ...FONTS.h2 }}>1</Text>
+                                        <Text style={{ ...FONTS.h2 }}>{getOrderQty(item.menuId)}</Text>
                                     </View>
                                     <TouchableOpacity
                                         style={{
@@ -156,6 +192,7 @@ const Restaurant = ({ route, navigation }) => {
                                             borderTopRightRadius: 25,
                                             borderBottomRightRadius: 25,
                                         }}
+                                        onPress={() => editOrder("+", item.menuId, item.price)}
                                     >
                                         <Text style={{ ...FONTS.body1 }}>+</Text>
                                     </TouchableOpacity>
@@ -256,6 +293,95 @@ const Restaurant = ({ route, navigation }) => {
             <View>
                 {
                     renderDots()
+                }
+                <View
+                    style={{
+                        backgroundColor: COLORS.white,
+                        borderTopLeftRadius: 40,
+                        borderTopRightRadius: 40
+                    }}
+                >
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            paddingVertical: SIZES.padding * 2,
+                            paddingHorizontal: SIZES.padding * 3,
+                            borderBottomColor: COLORS.lightGray2,
+                            borderBottomWidth: 1
+                        }}
+                    >
+                        <Text style={{ ...FONTS.h3 }}>Panier</Text>
+                        <Text style={{ ...FONTS.h3}}>45€</Text>
+                    </View>
+
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            paddingVertical: SIZES.padding * 2,
+                            paddingHorizontal: SIZES.padding * 3
+                        }}
+                    >
+                        <View style={{flexDirection: 'row'}} >
+                            <Image
+                                source={icons.pin}
+                                resizeMode="contain"
+                                style={{
+                                    width: 20,
+                                    height: 20,
+                                    tintColor: COLORS.darkgray
+                                }}
+                            />
+                            <Text style={{ marginLeft: SIZES.padding, ...FONTS.h4 }}>Location</Text>
+                        </View>
+                        <View style={{flexDirection: 'row'}}>
+                                <Image 
+                                    source={icons.master_card}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 20,
+                                        height: 20,
+                                        tintColor: COLORS.darkgray
+                                    }}
+                                />
+                                <Text style={{ marginLeft: SIZES.padding, ...FONTS.h4 }}>1428</Text>
+                        </View>
+                    </View>
+
+                    {/* Order Button */}
+                    <View
+                        style={{
+                            padding: SIZES.padding * 2,
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                        }}
+                    >
+                        <TouchableOpacity
+                            style={{
+                                width: SIZES.width * 0.9,
+                                padding: SIZES.padding,
+                                backgroundColor: COLORS.primary,
+                                alignItems: 'center',
+                                borderRadius: SIZES.radius
+                            }}
+                        >
+                            <Text style={{ color: COLORS.white, ...FONTS.h2}}>Commander</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                {isIphoneX() &&
+                    <View
+                        style={{
+                            postition: 'absolute',
+                            bottom: -34,
+                            left: 0,
+                            right: 0,
+                            height: 34,
+                            backgroundColor: COLORS.white
+                        }}
+                        >
+                    </View>
                 }
             </View>
         )
