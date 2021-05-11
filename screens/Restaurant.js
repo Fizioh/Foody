6cc1f90c6f5665129dcb9fb36150796353454a82
Fizioh <batrom@laposte.net>
2021-05-11 11:52:50 +0200

    onpress function to navigate into delivery map screen

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -384,6 +384,10 @@ const Restaurant = ({ route, navigation }) => {
                                 alignItems: 'center',
                                 borderRadius: SIZES.radius
                             }}
+                            onPress={() => navigation.navigate("OrderDelivery", {
+                                restaurant: restaurant,
+                                currentLocation: currentLocation
+                            })}
                         >
                             <Text style={{ color: COLORS.white, ...FONTS.h2}}>Commander</Text>
                         </TouchableOpacity>
