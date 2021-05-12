dca231fc756fe6a177eea47841e4c7b0b784f894
Fizioh <batrom@laposte.net>
2021-05-12 10:26:52 +0200

    integration Maps complete

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 
 const OrderDelivery = () => {
 
     function renderMap(){
-        <View>
-            
-        </View>
+        return (
+        <View style={styles.container}>
+     <MapView
+       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+       style={styles.map}
+       region={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
+     >
+     </MapView>
+   </View>
+   )
     }
 
     return (
@@ -15,5 +28,14 @@ const OrderDelivery = () => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      ...StyleSheet.absoluteFillObject,
+    },
+    map: {
+      ...StyleSheet.absoluteFillObject,
+    },
+   });
 
 export default OrderDelivery;
